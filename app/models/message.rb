1619ecf3d4b1edddfff756bd3e9c061e8323caf4
SamarQusai <samar.qusai@gmail.com>
2022-03-17 18:15:28 +0200

    RemoveAnalyzer'

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,22 +8,14 @@ class Message < ApplicationRecord
   validates :sequential_number, presence: true
   validates :sequential_number, uniqueness: {scope: :chat}
 
-  settings analysis: {
-    analyzer: {
-      search_analyzer: {
-        type: 'custom',
-        tokenizer: 'standard',
-        filter: ['lowercase']
-      }
-    }
-  } do
+  settings do
     mappings dynamic: false do
-      indexes :body, type: :text, analyzer: "search_analyzer"
+      indexes :body, type: :text
     end
   end
 
   def self.search_body(chat_id, body)
-    self.search({ 
+    self.search({
       query: {
         bool: {
            must: [
